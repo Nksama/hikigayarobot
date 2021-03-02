db8b57f96b71190740cf730e5c824fd49e3b2d99
Nksama <75163978+Nksama@users.noreply.github.com>
2021-03-02 20:38:53 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -1,4 +1,4 @@
-import json, time, os
+mport json, time, os
 from io import BytesIO
 
 from telegram import ParseMode, Message
@@ -335,9 +335,8 @@ def export_data(update, context):
     context.bot.sendDocument(
         current_chat_id,
         document=open("HikigayaRobot{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Sen
-uRobot-Backup` was specially made for notes."
-        .format(chat.title, chat_id, tgl)
+        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `HikigayaRobot-Backup` was specially made for notes."
+        .format(chat.title, chat_id, tgl),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
