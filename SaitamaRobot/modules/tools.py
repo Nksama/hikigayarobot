3ab951c1ee4a051d8625c3828481b6c45cf93815
Nksama <75163978+Nksama@users.noreply.github.com>
2021-03-14 11:04:10 +0530

    Create tools.py

---
+++ SaitamaRobot/modules/tools.py
@@ -0,0 +1,8 @@
+__help__ = f"""
+*Available Commands* *:*
+*Zipper*:
+• `/zip` *:* reply to a telegram file to compress it in .zip format
+• `/unzip` *:* reply to a telegram file to decompress it from the .zip format
+"""
+
+__mod_name__ = "Tools"
