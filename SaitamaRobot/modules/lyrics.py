f9ffcfb0892b9caac2c860e4c84e017ebaa3b716
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-02 10:41:37 +0530

    Update lyrics.py

--- SaitamaRobot/modules/lyrics.py
+++ SaitamaRobot/modules/lyrics.py
@@ -32,6 +32,16 @@ def lyrics(bot: Bot, update: Update, args):
                 caption="Message length exceeded max limit! Sending as a text file.")
         else:
             msg.reply_text(reply)
+
+__help__ = """
+Want to get the lyrics of your favorite songs straight from the app? This module is perfect for that!
+*Available commands:*
+ - /lyrics <song>: returns the lyrics of that song.
+ You can either enter just the song name or both the artist and song name.
+"""
+
+
+__mod_name__ = "Lyrics"
                 
         
 
