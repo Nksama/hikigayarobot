b69d0ad7999c58a7d61814eb34e782a82dc91dd2
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-04 16:24:11 +0530

    Update tools.py

--- SaitamaRobot/modules/tools.py
+++ SaitamaRobot/modules/tools.py
@@ -1,8 +1,16 @@
 __help__ = f"""
 *Available Commands* *:*
+
 *Zipper*:
 • `/zip` *:* reply to a telegram file to compress it in .zip format
 • `/unzip` *:* reply to a telegram file to decompress it from the .zip format
+ 
+*QR*
+
+• `/getqr <reply to image>`*:* Read QR code
+• `/makeqr <reply to text>`*:* Make QR code
+  
+
 """
 
 __mod_name__ = "Tools"
